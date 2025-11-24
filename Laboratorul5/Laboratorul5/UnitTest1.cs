f486bca8972dbb9aac1ca47eeeca689768fd3391
Ciprian PANZARU <ciprian.panzaru@smartgov.md>
2025-11-24 19:39:50 +0200

    Am adaugat laboratorul 5

---
+++ Laboratorul5/Laboratorul5/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Laboratorul5
+{
+	public class YouTubeSearchTest
+	{
+		private IWebDriver driver;
+
+		[SetUp]
+		public void Setup()
+		{
+			var options = new ChromeOptions();
+			options.AddArgument("--start-maximized");
+			driver = new ChromeDriver(options);
+		}
+
+		[Test]
+		public void SearchComputer_CheckYouTubeHeaderDisplayed()
+		{
+			// Deschide youtube.com
+			driver.Navigate().GoToUrl("https://www.youtube.com");
+
+			// Caută "computer"
+			var searchBox = driver.FindElement(By.Name("search_query"));
+			searchBox.SendKeys("computer");
+			searchBox.Submit();
+
+			// Verifică dacă antetul YouTube (logo) este afișat
+			var logo = driver.FindElement(By.Id("logo-icon"));
+			Assert.That(logo.Displayed, Is.True, "Antetul YouTube nu este afișat!");
+			Console.ReadLine();
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			driver?.Quit();
+			driver?.Dispose();
+		}
+	}
+}
